7c6a0c0b61a4bc632ea6c32c65c0a8e4309b23c3
Adedayo Okubanjo <dayookubanjo@Adedayos-MacBook-Pro-2.local>
2023-05-03 11:54:20 -0400

    Set Master

--- dags/ingest_dag.py
+++ dags/ingest_dag.py
@@ -1,7 +1,8 @@
 import airflow
 from datetime import timedelta
 from airflow import DAG
-from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator 
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.operators.bash_operator import BashOperator
 from airflow.utils.dates import days_ago
 
 default_args = {
@@ -26,21 +27,45 @@ Extract = SparkSubmitOperator(
 		application = "/opt/airflow/dags/spark_ingest_data.py",
 		conn_id= 'spark_local', 
 		task_id='spark_ingest_submit_task', 
-		dag=spark_dag
+		dag=spark_dag,
+        conf={'master':'spark://b89158def524:7077'} 
 		)
 
 Transform = SparkSubmitOperator(
 		application = "/opt/airflow/dags/spark_transform_data.py",
 		conn_id= 'spark_local', 
 		task_id='spark_transform_task', 
-		dag=spark_dag
+		dag=spark_dag,
+        conf={'master':'spark://b89158def524:7077'}  
 		)
 
 Model_Train = SparkSubmitOperator(
 		application = "/opt/airflow/dags/spark_train_model.py",
 		conn_id= 'spark_local', 
 		task_id='spark_train_model_task', 
-		dag=spark_dag
+		dag=spark_dag,
+        conf={'master':'spark://b89158def524:7077'}  
 		)
 
 Extract >> Transform >> Model_Train
+
+
+# Extract = BashOperator(
+# 		bash_command = "python3 /opt/airflow/dags/spark_ingest_data.py",
+# 		task_id='spark_ingest_submit_task', 
+# 		dag=spark_dag 
+# 		)
+
+# Transform = BashOperator(
+# 		bash_command = "python3 /opt/airflow/dags/spark_transform_data.py",  
+# 		task_id='spark_transform_task', 
+# 		dag=spark_dag 
+# 		)
+
+# Model_Train = BashOperator(
+# 		bash_command = "python3 /opt/airflow/dags/spark_train_model.py", 
+# 		task_id='spark_train_model_task', 
+# 		dag=spark_dag 
+# 		)
+
+# Extract >> Transform >> Model_Train
